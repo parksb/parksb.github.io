989b4dcd516cf6bb9ee82eec1e2782d4275a871c
ParkSB <parkgds@gmail.com>
2019-03-23 03:15:36 +0900

    feat[t]: shell scripts 업데이트

--- tools/build.sh
+++ tools/build.sh
@@ -1,17 +1,33 @@
 #! /bin/bash
 
-echo "Start build:"
+DIST="./parksb.github.io"
+CYAN="\033[36m"
+GREEN="\033[32m"
+WHITE="\033[0m"
 
-echo "Run node-sass..."
-echo "> ./node_modules/node-sass/bin/node-sass ./app/src/scss --output ./app/src/css"
+
+echo -e "${CYAN}Start build:\n"
+
+echo -e "${GREEN}Run node-sass...${WHITE}"
+echo -e "> ./node_modules/node-sass/bin/node-sass ./app/src/scss --output ./app/src/css\n"
 ./node_modules/node-sass/bin/node-sass ./app/src/scss --output ./app/src/css
 
-echo "Reset distribution directory..."
-echo "> rm -r ./parksb.github.io/*"
-rm -r ./parksb.github.io/*
+echo -e "\n${GREEN}Reset distribution directory...${WHITE}"
+echo "> rm -r ${DIST}/*"
+rm -r $DIST/*
 
-echo "Run parcel..."
-echo "> parcel build ./app/index.html -d ./parksb.github.io"
-parcel build ./app/index.html -d ./parksb.github.io
+echo -e "${GREEN}Copy static files to distribution directory...${WHITE}"
+echo "> cp ./app/static/robots.txt ${DIST}/robots.txt"
+cp ./app/static/robots.txt $DIST/robots.txt
+echo "> cp ./app/static/sitemap.xml ${DIST}/sitemap.xml"
+cp ./app/static/sitemap.xml $DIST/sitemap.xml
+echo "> cp ./app/static/naver16dc3632e707d380264984a55bd10171.html ${DIST}/naver16dc3632e707d380264984a55bd10171.html"
+cp ./app/static/naver16dc3632e707d380264984a55bd10171.html $DIST/naver16dc3632e707d380264984a55bd10171.html
+echo "> cp -r ./app/project ${DIST}/project"
+cp -r ./app/project $DIST/project
 
-echo "Done!"
+echo -e "\n${GREEN}Run parcel...${WHITE}"
+echo -e "> parcel build ./app/index.html -d ${DIST}\n"
+parcel build ./app/index.html -d $DIST
+
+echo -e "\n${CYAN}Done!${WHITE}"
--- tools/start.sh
+++ tools/start.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-echo "Start server:"
+CYAN="\033[36m"
+GREEN="\033[32m"
+WHITE="\033[0m"
 
-echo "Run node-sass..."
+echo -e "${CYAN}Start local server:\n"
+
+echo -e "${GREEN}Run node-sass...${WHITE}"
 echo "> ./node_modules/node-sass/bin/node-sass --watch ./app/src/scss --output ./app/src/css &"
 ./node_modules/node-sass/bin/node-sass --watch ./app/src/scss --output ./app/src/css &
 
-echo "Reset server directory..."
+echo -e "\n${GREEN}Reset server directory...${WHITE}"
 echo "> rm -r ./server/*"
 rm -r ./server/*
 
-echo "Run parcel..."
-echo "> parcel serve ./app/index.html -d ./server --open"
+echo -e "\n${GREEN}Run parcel...${WHITE}"
+echo -e "> parcel serve ./app/index.html -d ./server --open\n"
 parcel serve ./app/index.html -d ./server --open
