655003a783c839b54148f5ac7d3363a25289a197
Park Seong-beom <parkgds@gmail.com>
2019-03-28 22:57:11 +0900

    feat: 웹 접근성 개선

--- app/src/ts/index.ts
+++ app/src/ts/index.ts
@@ -8,15 +8,16 @@ import cover4Png from '../../assets/images/cover4.png';
 import cover5Png from '../../assets/images/cover5.png';
 import cover6Png from '../../assets/images/cover6.png';
 
-const emojiImges: string[] = [
-  cover0Png,
-  cover1Png,
-  cover2Png,
-  cover3Png,
-  cover4Png,
-  cover5Png,
-  cover6Png,
-];
+const emojiImges = {
+  0: { src: cover0Png, alt: '머리가 폭발하는 이모지.' },
+  1: { src: cover1Png, alt: '조이스틱 이모지.' },
+  2: { src: cover2Png, alt: '눈 이모지.' },
+  3: { src: cover3Png, alt: '생각하는 이모지.' },
+  4: { src: cover4Png, alt: '디자이너 이모지.' },
+  5: { src: cover5Png, alt: '프로그래머 이모지.' },
+  6: { src: cover6Png, alt: '웃는 이모지.' },
+};
 const emojiIndex: number = Math.floor(Math.random() * 7);
 
-document.getElementById('main-emoji').setAttribute('src', emojiImges[emojiIndex]);
+document.getElementById('main-emoji').setAttribute('src', emojiImges[emojiIndex].src);
+document.getElementById('main-emoji').setAttribute('alt', emojiImges[emojiIndex].alt);
