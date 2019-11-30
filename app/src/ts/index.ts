2f989d2c1e069db145b7b0a14997c29657e552e4
ParkSB <parkgds@gmail.com>
2019-11-30 18:44:51 +0900

    feat: github-calendar를 ghchart 이미지로 변경

--- app/src/ts/index.ts
+++ app/src/ts/index.ts
@@ -1,6 +1,1 @@
-import githubCalendar from 'github-calendar';
-
 import '../scss/index.scss';
-import 'github-calendar/dist/github-calendar.css';
-
-new githubCalendar('#heatmap-container', 'ParkSB', { summary_text: '@ParkSB', global_stats: false, responsive: true });
