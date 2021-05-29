b07b4014a3f3c0628ea7872369cb8cdc96a05ad7
parksb <parkgds@gmail.com>
2021-05-29 23:58:54 +0900

    feat: RSS 피드 발행 구현

--- tools/publish.ts
+++ tools/publish.ts
@@ -16,15 +16,25 @@ switch (target) {
     if (!mode || mode === 'all') {
       console.log('Publish all articles: ArticlePublisher.publishArticles()');
       ArticlePublisher.publishArticles();
+
       console.log('Publish sitemap: StaticPublisher.publishSitemap()');
       StaticPublisher.publishSitemap();
+
+      console.log('Publish rss: StaticPublisher.publishRSS()');
+      StaticPublisher.publishRSS();
+
       console.log('\x1b[36m%s\x1b[0m', 'Done!');
     } else if (!Number.isNaN(Number(mode))) {
       const id = Number(mode);
       console.log(`Publish article #${id}: ArticlePublisher.publishArticles(${id})`);
       ArticlePublisher.publishArticles(id);
+
       console.log('Publish sitemap: StaticPublisher.publishSitemap()');
       StaticPublisher.publishSitemap();
+
+      console.log('Publish rss: StaticPublisher.publishRSS()');
+      StaticPublisher.publishRSS();
+
       console.log('\x1b[36m%s\x1b[0m', 'Done!');
     } else {
       console.log('\x1b[31m%s\x1b[0m', `ERR! Unknown mode '${mode}'.`);
@@ -37,8 +47,10 @@ switch (target) {
     if (!mode || mode === 'all') {
       console.log('Publish all works: WorkPublisher.publishAllWorks()');
       WorkPublisher.publishAllWorks();
+
       console.log('Publish sitemap: StaticPublisher.publishSitemap()');
       StaticPublisher.publishSitemap();
+
       console.log('\x1b[36m%s\x1b[0m', 'Done!');
     } else {
       console.log('\x1b[31m%s\x1b[0m', `ERR! Unknown mode '${mode}'.`);
